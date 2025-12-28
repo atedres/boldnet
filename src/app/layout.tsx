0b62bc42ea2bd622cb92c4ab9a2be33cbfcfe223
ayman IT <aymanradi93@gmail.com>
2025-12-28 02:47:23 +0000

    UTILISE LES MEME FONTS QUE LE SITE QUE JET4AI DONN2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,6 +25,10 @@ export default function RootLayout({
           crossOrigin="anonymous"
         />
         <link
+          href="https://api.fontshare.com/v2/css?f[]=clash-display@700,500,600,400&display=swap"
+          rel="stylesheet"
+        />
+        <link
           href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800;900&display=swap"
           rel="stylesheet"
         />
