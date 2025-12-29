8689ce043f10450fb9c8868f40f88d4391649704
ayman IT <aymanradi93@gmail.com>
2025-12-29 08:55:20 +0000

    fait une petite animation pour les etapes ou chaque etape ait comme un e

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -95,15 +95,21 @@ export default {
             height: '0',
           },
         },
-        'fade-in-up': {
-          'from': { 
-            opacity: '0',
-            transform: 'translateY(20px)' 
+        'fill-in': {
+          from: { 
+            transform: 'scaleX(0)',
           },
-          'to': { 
-            opacity: '1',
-            transform: 'translateY(0)'
+          to: { 
+            transform: 'scaleX(1)',
           },
+        },
+        'fill-in-pseudo': {
+          from: {
+            transform: 'scaleX(0)',
+          },
+          to: {
+            transform: 'scaleX(1)',
+          }
         },
         scroll: {
           '0%': { transform: 'translateX(0)' },
@@ -113,7 +119,7 @@ export default {
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
-        'fade-in-up': 'fade-in-up 0.5s ease-out forwards',
+        'fill-in': 'fill-in 1.5s ease-out forwards',
         scroll: 'scroll 30s linear infinite',
       },
     },
