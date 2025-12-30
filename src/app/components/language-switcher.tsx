63021f9eac15809c0378f02b9a7ca841f1fe4b62
ayman IT <aymanradi93@gmail.com>
2025-12-30 17:22:59 +0000

    i want you to change the language changement icon to a flag of france if

--- src/app/components/language-switcher.tsx
+++ src/app/components/language-switcher.tsx
@@ -7,17 +7,41 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu';
-import { Globe } from 'lucide-react';
 import { useLanguage } from '@/app/context/language-context';
 
+const USFlag = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 25" className="w-6 h-auto rounded-sm">
+    <defs>
+      <clipPath id="a">
+        <path d="M0 0h50v25H0z" />
+      </clipPath>
+    </defs>
+    <g clipPath="url(#a)">
+      <path fill="#b22234" d="M0 0h50v25H0z" />
+      <path stroke="#fff" strokeWidth="2" d="M0 2.5h50M0 7.5h50M0 12.5h50M0 17.5h50M0 22.5h50" />
+      <path fill="#3c3b6e" d="M0 0h25v15H0z" />
+      <path fill="#fff" d="m2.5 1.5 1 2.75-2.6-.9h3.2l-2.6.9zm5 0 1 2.75-2.6-.9h3.2l-2.6.9zm5 0 1 2.75-2.6-.9h3.2l-2.6.9zm5 0 1 2.75-2.6-.9h3.2l-2.6.9zm5 0 1 2.75-2.6-.9h3.2l-2.6.9zM5 4.5l1 2.75-2.6-.9h3.2l-2.6.9zm5 0 1 2.75-2.6-.9h3.2l-2.6.9zm5 0 1 2.75-2.6-.9h3.2l-2.6.9zm5 0 1 2.75-2.6-.9h3.2l-2.6.9zM2.5 7.5l1 2.75-2.6-.9h3.2l-2.6.9zm5 0 1 2.75-2.6-.9h3.2l-2.6.9zm5 0 1 2.75-2.6-.9h3.2l-2.6.9zm5 0 1 2.75-2.6-.9h3.2l-2.6.9zm5 0 1 2.75-2.6-.9h3.2l-2.6.9zM5 10.5l1 2.75-2.6-.9h3.2l-2.6.9zm5 0 1 2.75-2.6-.9h3.2l-2.6.9zm5 0 1 2.75-2.6-.9h3.2l-2.6.9zm5 0 1 2.75-2.6-.9h3.2l-2.6.9z" />
+    </g>
+  </svg>
+);
+
+const FranceFlag = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 900 600" className="w-6 h-auto rounded-sm">
+    <path fill="#fff" d="M0 0h900v600H0z"/>
+    <path fill="#002654" d="M0 0h300v600H0z"/>
+    <path fill="#ed2939" d="M600 0h300v600H600z"/>
+  </svg>
+);
+
+
 export default function LanguageSwitcher() {
-  const { setLanguage } = useLanguage();
+  const { language, setLanguage } = useLanguage();
 
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button variant="ghost" size="icon">
-          <Globe className="h-[1.2rem] w-[1.2rem]" />
+          {language === 'fr' ? <FranceFlag /> : <USFlag />}
           <span className="sr-only">Toggle language</span>
         </Button>
       </DropdownMenuTrigger>
